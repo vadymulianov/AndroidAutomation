7ae2099cc6f92b0d68526c551022f17fce03e63c
Vadym Ulianov <vadymulianov@gmail.com>
2019-05-15 18:36:45 +0200

    Android Appium Test

---
+++ test_android_appium.py
@@ -0,0 +1,36 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from appium import webdriver
+
+
+class SearchProductsOnAndroid(unittest.TestCase):
+
+    def setUp(self):
+
+        desired_caps = {
+            'platformName': 'Android',
+            'deviceName': 'dea66a407d73',
+            'platformVersion': '6.0.1',
+            'browserName': 'Chrome'
+        }
+
+        # To connect to Appium server use RemoteWebDriver and pass desired capabilities
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+        self.driver.get("http://google.com")
+
+    def test_search(self):
+
+        self.driver.find_element_by_name('q').send_keys('Appium')
+        self.driver.find_element_by_name('q').send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(20)
+
+    def tearDown(self):
+
+        self.driver.close()
+
+
+if __name__=='__main__':
+    unittest.main(verbosity=2)
+
+
